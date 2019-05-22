4b53fcd80d5646656119882ec8a938b19e66d0af
jhonrsalcedo <jhonfor@gmail.com>
2019-05-22 00:08:54 -0500

    add style nav header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,7 @@ import './Header.css'
 const Header = () => {
     return ( 
         <header className="col-12 text-center">
-            <div className="p-bottom">
+            <div className="header-bottom">
             <h1>Our Team</h1>
             <h3>These are the people living the "Junior developer for life" way</h3>
             </div>
