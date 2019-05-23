c7ca0c6dff3e1ce01ede822b7646da5dfa7e8bbb
jhonrsalcedo <jhonfor@gmail.com>
2019-05-23 11:27:49 -0500

    updated compnet footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,7 @@ import './Footer.css';
 const Footer = () => {
     return ( 
 
-        <footer className="footer mt-auto py-3 fixed-bottom text-center">
+        <footer className="footer mt-auto py-3 text-center">
         <div className="container">
           <span className=" text-white">Condor Labs Copyright 2018 - All rights reserved</span>
         </div>
